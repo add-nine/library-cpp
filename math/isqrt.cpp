0775d10e400334c13aec6aaa55e895d9d13b7225
add-nine <addninetonull@gmail.com>
2025-03-16 23:52:21 +0900

    isqrt

---
+++ math/isqrt.cpp
@@ -0,0 +1,15 @@
+template <class T>
+T isqrt(T n) {
+    assert(n >= 0);
+    if (n <= 1) return n;
+    T x = n, y = (x + n / x) / 2;
+    while (y < x) {
+        x = y;
+        y = (x + n / x) / 2;
+    }
+    return x;
+}
+
+/**
+ * @brief computes integer sqrt
+ */
